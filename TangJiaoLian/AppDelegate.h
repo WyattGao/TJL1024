46f39c1c22a4868508050c6ca00b43f0aaaa6c9b
高临原 <gaoLinYuan@glymac.local>
2017-03-14 18:20:10 +0800

    Initial Commit

---
+++ TangJiaoLian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TangJiaoLian
+//
+//  Created by 高临原 on 2017/3/14.
+//  Copyright © 2017年 高临原. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TangJiaoLian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TangJiaoLian
+//
+//  Created by 高临原 on 2017/3/14.
+//  Copyright © 2017年 高临原. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
